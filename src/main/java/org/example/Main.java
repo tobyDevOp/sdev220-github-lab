5f643512db4760bd20844a78abc212d01c17ed5d
tobygoetz <goetz.tobiah@student.greenriver.edu>
2023-06-08 17:05:02 -0700

    Added documentation to the project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+/**
+ * This is the entry point to my sample app
+ *
+ * @author tobygoetz
+ * @version 1.0
+ */
 public class Main
 {
+    /**
+     * Starts the application
+     * @param args command-line args (not used)
+     */
     public static void main(String[] args)
     {
         System.out.println("Hello world, from SDEV 220");
